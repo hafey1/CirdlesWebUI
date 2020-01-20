146f793416e1550c32a5b72bac4278a9029ff0f3
Julius Walton <waltonjj@g.cofc.edu>
2020-01-20 17:37:58 -0500

    added about and help dropdown

--- src/scenes/Mars/index.js
+++ src/scenes/Mars/index.js
@@ -1,8 +1,13 @@
 import { connect } from "react-redux";
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Route, Link, Switch } from "react-router-dom";
 import { signOutAction } from "../../actions/mars";
 import HomePage from "./components/homepage";
+import SimpleMenu from "./components/simpleMenu";
+
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 //scenes
 import LogIn from "./components/login";
 
@@ -13,11 +18,53 @@ import noRequireAuth from "./components/higherOrderComponents/no_require_auth";
 //Containers
 import Upload from "./components/upload";
 import Mapping from "./components/mapping";
+import { NativeSelect } from "@material-ui/core";
 
 //CSS
 //import "./index.css";
+/*function simpleMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <div>
+      <Button
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        Open Menu
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
+}*/
 
 class MarsPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      anchorEl: null
+    };
+
+    //this.simpleMenu = this.simpleMenu.bind(this);
+  }
   navbarLinks() {
     //Return these links if the user is authenticated
     if (this.props.authenticated) {
@@ -33,14 +80,23 @@ class MarsPage extends Component {
               Mapping
             </Link>
           </li>
+
           <li className="nav-item">
             <button
               type="button"
-              className="nav-link"
+              style={{
+                background: "#f8f9fa",
+                color: "#007bff",
+                borderColor: "#f8f9fa"
+              }}
+              className="btn btn-primary btn-md nav-link"
               onClick={() => this.props.signOutAction()}
             >
               Log Out
             </button>
+          </li>
+          <li className="nav-item">
+            <SimpleMenu />
           </li>
         </ul>
       );
@@ -48,17 +104,65 @@ class MarsPage extends Component {
       return (
         <ul className="nav">
           <li className="nav-item">
-            <Link className="nav-link" to="/mars/login">
+            <Link
+              style={{ marginTop: ".5rem" }}
+              className="nav-link"
+              to="/mars/login"
+            >
               Log In
             </Link>
+          </li>
+          <li className="nav-item">
+            <div className="nav-link">
+              <SimpleMenu />
+            </div>
           </li>
         </ul>
       );
     }
-
     //Default links to show if the user is not authenticated
   }
 
+  /**  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  }; */
+
+  /*simpleMenu() {
+    const handleClick = event => {
+      this.setState({ anchorEl: event.currentTarget });
+    };
+
+    const handleClose = () => {
+      this.setState({ anchorEl: null });
+    };
+
+    return (
+      <div>
+        <Button
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+        >
+          Open Menu
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={this.state.anchorEl}
+          keepMounted
+          open={Boolean(this.state.anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>Profile</MenuItem>
+          <MenuItem onClick={handleClose}>My account</MenuItem>
+          <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
+      </div>
+    );
+  }*/
   render() {
     return (
       <div>
