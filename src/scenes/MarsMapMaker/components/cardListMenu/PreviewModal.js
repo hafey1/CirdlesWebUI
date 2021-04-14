b7cfde59f8beb99afeece46f97e5f6663c307e85
Robert Niggebrugge <38027049+hafey1@users.noreply.github.com>
2021-04-14 12:35:15 -0400

    switched order on preview modal (#88)
    * switched order
    
    * added value types to multivalue display
    
    * added forced value appearance
    
    * cleaned up logs

--- src/scenes/MarsMapMaker/components/cardListMenu/PreviewModal.js
+++ src/scenes/MarsMapMaker/components/cardListMenu/PreviewModal.js
@@ -1,14 +1,43 @@
 import Modal from "react-modal";
+import { connect } from "react-redux";
 import React, { useState } from "react";
 import { dialogFilter } from "../../util/helper";
+
+import {
+  findFirstValueBySesarTitle
+} from "../../util/helper";
 
 export const PreviewModal = props => {
   const [modalShow, setModalShow] = useState(false);
   const previewMapping = entry => {
+    
+    let localVals = entry[1]
+
+    if (localVals[0].includes(" : ")) {
+      
+      localVals = localVals.map(ele => {
+        let replaceValue = "[VALUE]"
+        // digit value
+        if( ele.match(/[\d]/)) {
+          replaceValue = "NNN"
+        }
+        // no value
+        else if (ele.match(/^.*([  ]$)/)) {
+          replaceValue = "Not_Provided"
+        }
+
+        return ele.replace(/(?<= : )(.*)/, replaceValue)
+      })
+    }
+    let localAtt = localVals.join(";");
+    //displays forced value
+    if (localAtt.includes("<METADATA"))
+      localAtt = localAtt.replace(/(<.*>)/, findFirstValueBySesarTitle(props.ent, entry[0]));
+  
     return (
       <tr key={entry[0]}>
+        <td>{localAtt}</td>
         <td>{entry[0]}</td>
-        <td>{entry[1].join(";")}</td>
       </tr>
     );
   };
@@ -42,3 +71,15 @@ export const PreviewModal = props => {
     </div>
   );
 };
+
+
+const mapStateToProps = state => {
+  return {
+    ent: state.marsMapMaker.entries,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(PreviewModal);
